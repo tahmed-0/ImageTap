326cb1cb76a6f7ae72a149ed2f55afc0a8113894
Tufael Ahmed <tahmed0113@gmail.com>
2021-07-15 23:05:57 -0400

    initial

--- anthing/ViewController.swift
+++ anthing/ViewController.swift
@@ -6,14 +6,47 @@
 //
 
 import UIKit
+import CoreMotion
 
 class ViewController: UIViewController {
+    
+    var randomImage : Int = 0
+    var backgroundImage: Int = 0;
+    
+    let imageArray = ["1","2","3","4","5","6","7"]
+    
 
+    @IBOutlet weak var Image: UIImageView!
+    
+    @IBOutlet weak var BackImage: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    func updateImage() {
+        randomImage = Int(arc4random_uniform(6))
+        Image.image = UIImage(named: imageArray[randomImage])
+        
+        backgroundImage = Int(arc4random_uniform(6))
+        BackImage.image = UIImage(named: imageArray[backgroundImage])
+       
+    }
+    
+   
+    @IBAction func TAP(_ sender: Any) {
+        updateImage()
+        
+    }
+    override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        updateImage()
+    }
+    
+    }
+    
 
-
-}
+    
+    
 
